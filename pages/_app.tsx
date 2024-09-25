08128b2f0a1c67db204b94b6b6e85a5d42ab7410
Kason Shiroma <kasons@hawaii.edu>
2024-09-24 17:21:59 -1000

    testing react-pdf build errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,30 @@ import Layout from "@/Components/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+if (typeof Promise.withResolvers === 'undefined') {
+  if (window) {
+    // @ts-expect-error This does not exist outside of polyfill which this is doing
+    window.Promise.withResolvers = function () {
+      let resolve, reject;
+      const promise = new Promise((res, rej) => {
+        resolve = res;
+        reject = rej;
+      });
+      return { promise, resolve, reject };
+    };
+  } else {
+    // @ts-expect-error This does not exist outside of polyfill which this is doing
+    global.Promise.withResolvers = function () {
+      let resolve, reject;
+      const promise = new Promise((res, rej) => {
+        resolve = res;
+        reject = rej;
+      });
+      return { promise, resolve, reject };
+    };
+  }
+}
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
