824495350391dba7d91aa00bda750d5ece39a175
Kason Shiroma <kasons@hawaii.edu>
2024-09-25 16:06:09 -1000

    Update ResumeSkeleton.tsx

--- Components/ResumeSkeleton.tsx
+++ Components/ResumeSkeleton.tsx
@@ -3,91 +3,91 @@ import React from 'react'
 const ResumeSkeleton = () => {
   return (
     <div className='flex items-center'>
-        <div className='flex flex-col bg-slate-100 text-left w-[90%] mx-auto p-16 gap-4 animate-pulse'>
+        <div className='flex flex-col bg-slate-100 text-left w-[90%] mx-auto p-8 md:p-16 gap-4 animate-pulse'>
             <div className='bg-slate-300 w-[40%] h-20 mb-12'/>
             <div className='flex flex-row gap-1 items-center gap-4'>
-                <div className='bg-slate-300 w-[20%] h-8'/>
-                <div className='bg-slate-100 w-[30%] h-8'/>
-                <div className='bg-slate-300 w-[20%] h-8'/>
-                <div className='bg-slate-100 w-[30%] h-8'/>
+                <div className='bg-slate-300 w-full md:w-[20%] h-52 md:h-8'/>
+                <div className='bg-slate-100 hidden md:block md:w-[30%] md:h-8'/>
+                <div className='bg-slate-300 hidden md:block md:w-[20%] md:h-8'/>
+                <div className='bg-slate-100 hidden md:block md:w-[30%] md:h-8'/>
             </div>
             <div className='flex flex-row gap-1 items-center gap-4'>
-                <div className='bg-slate-300 w-[30%] h-8'/>
-                <div className='bg-slate-100 w-[20%] h-8'/>
-                <div className='bg-slate-300 w-[30%] h-8'/>
-                <div className='bg-slate-100 w-[20%] h-8'/>
+                <div className='bg-slate-300 w-full md:w-[30%] h-52 md:h-8'/>
+                <div className='bg-slate-100 hidden md:block md:w-[20%] md:h-8'/>
+                <div className='bg-slate-300 hidden md:block md:w-[30%] md:h-8'/>
+                <div className='bg-slate-100 hidden md:block md:w-[20%] md:h-8'/>
             </div>
             <div className='flex flex-row gap-1 items-center gap-4'>
-                <div className='bg-slate-300 w-[40%] h-8'/>
-                <div className='bg-slate-100 w-[10%] h-8'/>
-                <div className='bg-slate-300 w-[50%] h-8'/>
-                <div className='bg-slate-300 w-[0%] h-8'/>
+                <div className='bg-slate-300 hidden md:block md:w-[40%] md:h-8'/>
+                <div className='bg-slate-100 hidden md:block md:w-[10%] md:h-8'/>
+                <div className='bg-slate-300 hidden md:block md:w-[50%] md:h-8'/>
+                <div className='bg-slate-300 hidden md:block md:w-[0%] md:h-8'/>
             </div>
             <div className='flex flex-row gap-1 items-center gap-4'>
-                <div className='bg-slate-300 w-[40%] h-8'/>
-                <div className='bg-slate-100 w-[10%] h-8'/>
-                <div className='bg-slate-300 w-[50%] h-8'/>
-                <div className='bg-slate-300 w-[0%] h-8'/>
+                <div className='bg-slate-300 hidden md:block md:w-[40%] md:h-8'/>
+                <div className='bg-slate-100 hidden md:block md:w-[10%] md:h-8'/>
+                <div className='bg-slate-300 hidden md:block md:w-[50%] md:h-8'/>
+                <div className='bg-slate-300 hidden md:block md:w-[0%] md:h-8'/>
             </div>
             <div className='flex flex-row gap-1 items-center gap-4'>
-                <div className='bg-slate-300 w-[10%] h-8'/>
-                <div className='bg-slate-100 w-[40%] h-8'/>
-                <div className='bg-slate-300 w-[50%] h-8'/>
-                <div className='bg-slate-300 w-[0%] h-8'/>
+                <div className='bg-slate-300 hidden md:block md:w-[10%] md:h-8'/>
+                <div className='bg-slate-100 hidden md:block md:w-[40%] md:h-8'/>
+                <div className='bg-slate-300 hidden md:block md:w-[50%] md:h-8'/>
+                <div className='bg-slate-300 hidden md:block md:w-[0%] md:h-8'/>
             </div>
             <div className='flex flex-row gap-1 items-center gap-4'>
-                <div className='bg-slate-300 w-[15%] h-8'/>
-                <div className='bg-slate-100 w-[35%] h-8'/>
-                <div className='bg-slate-300 w-[50%] h-8'/>
-                <div className='bg-slate-300 w-[0%] h-8'/>
+                <div className='bg-slate-300 hidden md:block md:w-[15%] md:h-8'/>
+                <div className='bg-slate-100 hidden md:block md:w-[35%] md:h-8'/>
+                <div className='bg-slate-300 hidden md:block md:w-[50%] md:h-8'/>
+                <div className='bg-slate-300 hidden md:block md:w-[0%] md:h-8'/>
             </div>
             <div className='flex flex-row gap-1 items-center gap-4'>
-                <div className='bg-slate-300 w-[15%] h-8'/>
-                <div className='bg-slate-100 w-[35%] h-8'/>
-                <div className='bg-slate-300 w-[50%] h-8'/>
-                <div className='bg-slate-300 w-[0%] h-8'/>
+                <div className='bg-slate-300 hidden md:block md:w-[15%] md:h-8'/>
+                <div className='bg-slate-100 hidden md:block md:w-[35%] md:h-8'/>
+                <div className='bg-slate-300 hidden md:block md:w-[50%] md:h-8'/>
+                <div className='bg-slate-300 hidden md:block md:w-[0%] md:h-8'/>
             </div>
             <div className='flex flex-row gap-1 items-center gap-4'>
-                <div className='bg-slate-300 w-[15%] h-8'/>
-                <div className='bg-slate-100 w-[35%] h-8'/>
-                <div className='bg-slate-300 w-[20%] h-8'/>
-                <div className='bg-slate-100 w-[30%] h-8'/>
+                <div className='bg-slate-300 hidden md:block md:w-[15%] md:h-8'/>
+                <div className='bg-slate-100 hidden md:block md:w-[35%] md:h-8'/>
+                <div className='bg-slate-300 hidden md:block md:w-[20%] md:h-8'/>
+                <div className='bg-slate-100 hidden md:block md:w-[30%] md:h-8'/>
             </div>
             <div className='flex flex-row gap-1 items-center gap-4'>
-                <div className='bg-slate-300 w-[10%] h-8'/>
-                <div className='bg-slate-100 w-[40%] h-8'/>
-                <div className='bg-slate-300 w-[30%] h-8'/>
-                <div className='bg-slate-100 w-[20%] h-8'/>
+                <div className='bg-slate-300 hidden md:block md:w-[10%] md:h-8'/>
+                <div className='bg-slate-100 hidden md:block md:w-[40%] md:h-8'/>
+                <div className='bg-slate-300 hidden md:block md:w-[30%] md:h-8'/>
+                <div className='bg-slate-100 hidden md:block md:w-[20%] md:h-8'/>
             </div>
             <div className='flex flex-row gap-1 items-center gap-4'>
-                <div className='bg-slate-300 w-[30%] h-8'/>
-                <div className='bg-slate-100 w-[20%] h-8'/>
-                <div className='bg-slate-300 w-[50%] h-8'/>
-                <div className='bg-slate-300 w-[0%] h-8'/>
+                <div className='bg-slate-300 hidden md:block md:w-[30%] md:h-8'/>
+                <div className='bg-slate-100 hidden md:block md:w-[20%] md:h-8'/>
+                <div className='bg-slate-300 hidden md:block md:w-[50%] md:h-8'/>
+                <div className='bg-slate-300 hidden md:block md:w-[0%] md:h-8'/>
             </div>
             <div className='flex flex-row gap-1 items-center gap-4'>
-                <div className='bg-slate-300 w-[30%] h-8'/>
-                <div className='bg-slate-100 w-[20%] h-8'/>
-                <div className='bg-slate-300 w-[50%] h-8'/>
-                <div className='bg-slate-300 w-[0%] h-8'/>
+                <div className='bg-slate-300 hidden md:block md:w-[30%] md:h-8'/>
+                <div className='bg-slate-100 hidden md:block md:w-[20%] md:h-8'/>
+                <div className='bg-slate-300 hidden md:block md:w-[50%] md:h-8'/>
+                <div className='bg-slate-300 hidden md:block md:w-[0%] md:h-8'/>
             </div>
             <div className='flex flex-row gap-1 items-center gap-4'>
-                <div className='bg-slate-300 w-[30%] h-8'/>
-                <div className='bg-slate-100 w-[20%] h-8'/>
-                <div className='bg-slate-300 w-[50%] h-8'/>
-                <div className='bg-slate-300 w-[0%] h-8'/>
+                <div className='bg-slate-300 hidden md:block md:w-[30%] md:h-8'/>
+                <div className='bg-slate-100 hidden md:block md:w-[20%] md:h-8'/>
+                <div className='bg-slate-300 hidden md:block md:w-[50%] md:h-8'/>
+                <div className='bg-slate-300 hidden md:block md:w-[0%] md:h-8'/>
             </div>
             <div className='flex flex-row gap-1 items-center gap-4'>
-                <div className='bg-slate-300 w-[30%] h-8'/>
-                <div className='bg-slate-100 w-[20%] h-8'/>
-                <div className='bg-slate-300 w-[50%] h-8'/>
-                <div className='bg-slate-300 w-[0%] h-8'/>
+                <div className='bg-slate-300 hidden md:block md:w-[30%] md:h-8'/>
+                <div className='bg-slate-100 hidden md:block md:w-[20%] md:h-8'/>
+                <div className='bg-slate-300 hidden md:block md:w-[50%] md:h-8'/>
+                <div className='bg-slate-300 hidden md:block md:w-[0%] md:h-8'/>
             </div>
             <div className='flex flex-row gap-1 items-center gap-4'>
-                <div className='bg-slate-300 w-[30%] h-8'/>
-                <div className='bg-slate-100 w-[20%] h-8'/>
-                <div className='bg-slate-300 w-[50%] h-8'/>
-                <div className='bg-slate-300 w-[0%] h-8'/>
+                <div className='bg-slate-300 hidden md:block md:w-[30%] md:h-8'/>
+                <div className='bg-slate-100 hidden md:block md:w-[20%] md:h-8'/>
+                <div className='bg-slate-300 hidden md:block md:w-[50%] md:h-8'/>
+                <div className='bg-slate-300 hidden md:block md:w-[0%] md:h-8'/>
             </div>
         </div>
     </div>
