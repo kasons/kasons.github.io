8175a6e7854a1effeb9403be63441dea609b6039
Kason Shiroma <kasons@hawaii.edu>
2024-09-25 16:26:43 -1000

    Added success message to form

--- Components/EmailSection.tsx
+++ Components/EmailSection.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react'
 
 const EmailSection = () => {
+    const [show, setShow] = useState(false);
     const handleSubmit = async (e: React.ChangeEvent<any>) => {
         e.preventDefault();
 
@@ -21,37 +22,42 @@ const EmailSection = () => {
         if (error) {
             console.log(error);
             return;
+        } else {
+            setShow(!show);
         }
     }
     return (
-        <form onSubmit={handleSubmit}>
-            <div className='sm:flex gap-6 mb-6'>
-                <input
-                    name='name'
-                    type='name'
-                    id='name'
+        !show ?
+            <form onSubmit={handleSubmit}>
+                <div className='sm:flex gap-6 mb-6'>
+                    <input
+                        name='name'
+                        type='name'
+                        id='name'
+                        required
+                        className='bg-slate-300 border border-[#33353F] placeholder-slate-600 placeholder:italic text-[#212226] text-lg rounded block w-full md:w-[20vw] 2xl:w-[17vw] py-3 px-4 mb-6 sm:mb-0'
+                        placeholder='Name'
+                    />
+                    <input
+                        name='email'
+                        type='email'
+                        id='email'
+                        required
+                        className='bg-slate-300 border border-[#33353F] placeholder-slate-600 placeholder:italic text-[#212226] text-lg rounded block w-full md:w-[20vw] 2xl:w-[17vw] py-3 px-4'
+                        placeholder='Email'
+                    />
+                </div>
+                <textarea
+                    name='message'
+                    id='message'
                     required
-                    className='bg-slate-300 border border-[#33353F] placeholder-slate-600 placeholder:italic text-[#212226] text-lg rounded block w-full md:w-[20vw] 2xl:w-[17vw] py-3 px-4 mb-6 sm:mb-0'
-                    placeholder='Name'
+                    className='bg-slate-300 border border-[#33353F] placeholder-slate-600 placeholder:italic text-[#212226] text-lg rounded block w-full py-3 px-4 min-h-44 mb-6'
+                    placeholder='Message'
                 />
-                <input
-                    name='email'
-                    type='email'
-                    id='email'
-                    required
-                    className='bg-slate-300 border border-[#33353F] placeholder-slate-600 placeholder:italic text-[#212226] text-lg rounded block w-full md:w-[20vw] 2xl:w-[17vw] py-3 px-4'
-                    placeholder='Email'
-                />
-            </div>
-            <textarea
-                name='message'
-                id='message'
-                required
-                className='bg-slate-300 border border-[#33353F] placeholder-slate-600 placeholder:italic text-[#212226] text-lg rounded block w-full py-3 px-4 min-h-44 mb-6'
-                placeholder='Message'
-            />
-            <button type='submit' className='bg-[linear-gradient(35deg,white_50%,#303136_50%)] bg-animation bg-right-bottom transition-all duration-[0.3s] ease-[ease] hover:bg-left-top hover:text-black text-white py-3 text-xl w-32 font-medium rounded inline-block'>Send</button>
-        </form>
+                <button type='submit' className='bg-[linear-gradient(35deg,white_50%,#303136_50%)] bg-animation bg-right-bottom transition-all duration-[0.3s] ease-[ease] hover:bg-left-top hover:text-black text-white py-3 text-xl w-32 font-medium rounded inline-block'>Send</button>
+            </form>
+        :
+        <div className='bg-gray-700 rounded w-full h-fit md:w-[50vw] 2xl:w-[34vw] text-slate-300 text-semibold text-lg p-10 text-center'>Your message has been successfully sent. Thanks for reaching out!</div>
     )
 }
 
